ce8e304c52e6daa22394f76084fa3297f2d5ca0c
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-06-30 09:41:49 -0500

    Working on Adding database to the spring boot project

---
+++ Chapter_4_Adding_Database_Access_To_Your_Spring_Boot_App/sbur-rest-demo/src/main/java/com/thehecklers/sburrestdemo/controllers/RestApiDemoController.java
@@ -0,0 +1,58 @@
+package com.thehecklers.sburrestdemo.controllers;
+
+import com.thehecklers.sburrestdemo.domain.Coffee;
+import com.thehecklers.sburrestdemo.repository.CoffeeRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/coffees")
+public class RestApiDemoController {
+
+    private final CoffeeRepository coffeeRepository;
+
+
+    public RestApiDemoController(CoffeeRepository coffeeRepository) {
+        this.coffeeRepository = coffeeRepository;
+
+        this.coffeeRepository.saveAll(List.of(
+                new Coffee("Café Cereza"),
+                new Coffee("Café Ganador"),
+                new Coffee("Café Lareño"),
+                new Coffee("Café Tres Pontas")
+        ));
+    }
+
+    @GetMapping
+    Iterable<Coffee> getCoffees(){
+        return coffeeRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Optional<Coffee> getCoffeeById(@PathVariable String id){
+        return coffeeRepository.findById(id);
+    }
+
+    @PostMapping
+    Coffee postCoffee(@RequestBody Coffee coffee){
+        return coffeeRepository.save(coffee);
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<Coffee> putCoffee(@PathVariable String id,
+                                     @RequestBody Coffee coffee) {
+        return (!coffeeRepository.existsById(id))
+                ? new ResponseEntity<>(coffeeRepository.save(coffee),
+                HttpStatus.CREATED)
+                : new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    void deleteCoffee(@PathVariable String id){
+        coffeeRepository.deleteById(id);
+    }
+}
