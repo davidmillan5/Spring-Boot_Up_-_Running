1d98c014426b637d13a999bab287be4575a643b2
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-06-30 19:52:56 -0500

    Starting Chapter 6

--- Chapter_6_Really_Digging_Into_Data/sbur-rest-demo/src/main/java/com/thehecklers/sburrestdemo/repository/CoffeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.thehecklers.sburrestdemo.repository;
-
-import com.thehecklers.sburrestdemo.domain.Coffee;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CoffeeRepository extends CrudRepository<Coffee,String> {
-}
